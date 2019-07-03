059c31cf3a5cb50fb7d1fc255e8a11a474aa8f04
sj_rifat <45694548+sjRifat@users.noreply.github.com>
2019-07-03 16:17:01 +0600

    Create F.cpp

---
+++ F.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define		pb              push_back
+#define		PB              pop_back
+#define		nn              "\n"
+#define		all(p)          p.begin(),p.end()
+#define		ss              ' '
+#define		gcd(a,b)        __gcd(a,b)
+#define		lcm(a,b)        (a*b)/gcd(a,b)
+#define		pi              acos(-1.0)
+typedef		long long       ll;
+typedef		vector<ll>      vll;
+
+int main()
+{
+    ll a,b,c,i,j,t,k,m,n,o,even=0,odd=0,x,y,z;
+    cout<<"Ma Hu Nepali Babu"<<nn;
+
+    return 0;
+}
