22e5d9aefe1d47a5b94a74ec37c91edcf8a9a548
sj_rifat <45694548+sjRifat@users.noreply.github.com>
2019-07-03 16:15:31 +0600

    Create B.cpp

---
+++ B.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define		pb              push_back
+#define		PB              pop_back
+#define		nn              "\n"
+#define		all(p)          p.begin(),p.end()
+#define		ss              ' '
+#define		gcd(a,b)        __gcd(a,b)
+#define		lcm(a,b)        (a*b)/gcd(a,b)
+#define		pi              acos(-1.0)
+typedef		long long       ll;
+typedef		vector<ll>      vll;
+
+int main()
+{
+    ll a,b,c=0,i,j,t,k,m,n,o,ar[1010],x,y,z;
+    cin>>a>>b>>c;
+    cout<<a*b*c*1000;
+
+    return 0;
+}
